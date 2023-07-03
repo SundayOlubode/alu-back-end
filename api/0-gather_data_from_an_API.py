ef84dffd842dbcf7f051b80688c49925b773f776
Samuel <s.olubode@alustudent.com>
2023-07-03 16:19:04 +0000

    fix: import request package

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,16 +2,18 @@
 """Module"""
 
 import requests
-import sys
+from sys import argv
 
 
 """Module"""
 
 if __name__ == '__main__':
-    employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+    """IF SCRIPT IS NOT RUN AS MODULE"""
+
+    employee_id = argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}"\
         .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/"\
         .format(employee_id)
 
     user_info = requests.request('GET', user_url).json()
@@ -20,6 +22,7 @@ if __name__ == '__main__':
     employee_name = user_info["name"]
     task_completed = list(filter(lambda obj:
                                  (obj["completed"] is True), todos_info))
+    
     number_of_done_tasks = len(task_completed)
     total_number_of_tasks = len(todos_info)
 
